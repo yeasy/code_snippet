c59ee95cf98f73921b4e0adac26304c543f97dcd
Baohua Yang <yangbaohua@gmail.com>
2016-05-09 15:57:02 +0800

    monir optimization

--- README.md
+++ README.md
@@ -7,100 +7,100 @@ Most of them are written by me, while some one may be collected from Internet or
 
 All files are free to use, distribute or modify, with keeping the declarition part on the file head. However, the authors WILL NOT be responsible for any result by these files. If you think there might be some one affecting your privilege, please let me know.
 
-##autopreview 
+## autopreview
 A light script plugin for Vim that could automatically preview the definition of variables or functions in a tiny window like source insight. Also be hosted at the [vim homepage](http://www.vim.org/scripts/script.php?script_id=2228).
 
-##algorithm
+## algorithm
 Some code blocks for test algorithms, such as sorting, searching, etc..
 
-##bf-gdt
+## bf-gdt
 Example to show how to use the bloom-filter based gdt data structure.
 
-##cpu
+## cpu
 Get overall cpu utilization at linux platform.
 
-##Docker
+## Docker
 Some docker related scripts and tools.
-###docker\_cleanup
+### docker\_cleanup
 Do some cleanup work on Docker Host, e.g., removing stopped containers and untagged images.
 
-###ovs\_perf
+### ovs\_perf
 Test two containers connecting through OpenvSwitch with iperf
 
-###save\_all\_images
+### save\_all\_images
 Save all local Docker images into tar.gz files.
 
-###restore\_images
+### restore\_images
 This script will load into the tar.gz files as images.
 
-##ether\_bridge
+## ether\_bridge
 Connect to network interface and forward ethernet packets between them bidirectionally, supporting options.
 
-##getIP
+## getIP
 Some scripts to get local ip address of the network interfaces.
 
-##getopt
+## getopt
 Show how to use getopt functions.
 
-##io
+## io
 Test read and write functions.
 
-##json
+## json
 Example to show how to use json encoding/decoding in python.
 
-##mcast
+## mcast
 Test the multi-cast packet sending and receiving, using a thread separately.
 
-##Makefile.template
+## Makefile.template
 Simple template of Makefile.
 
-##netlink
+## netlink
 Test bidirectional communication between userspace and kernelspace, using low-level netlink APIs in the userspace, and libnl APIs in the kernelspace.
 
-##python
+## [python](python)
 Simple codes to show how to use python libraries, e.g., regrex, encoding.
-###amqp
+### amqp
 Demo the usage of AMQP by the kombu and pika.
-### concurrency
+###  concurrency
 Some scripts on concurrency programming, such as threading, eventlet...
-### gitbook\_gen
+###  gitbook\_gen
 Generate an initialized gitbook project based on the given source code path.
-###httpserver
+### httpserver
 A simple webserver by python, which tracks request number of each visitor's IP address.
-###mqtt
+### mqtt
 Demo the usage of MQTT.
-###prefixCutter
+### prefixCutter
 Calculate the cut prefixes by given two prefixes.
-###remote\_watcher
+### remote\_watcher
 Watch the specified file on remote, trigger action when there finds keyword in it.
-###sm\_watcher
+### sm\_watcher
 Track (and filter) the board articles on the given url (newsmth now), and show them in a webpage in realtime (using jquery and HTML5 websocket).
-###wsgi
+### wsgi
 A demo to show how to use wsgi and paste deployment to provide a web service. 
 Run `python server.py` and get the version information by visiting localhost:8080.
 
-##remote
-###remoteAuth.sh
+## [remote](remote)
+### remoteAuth.sh
 An ssh authorized script, which enables you to login into remote host automatically without passwd inputing.
-###pull\_agent.sh
+### pull\_agent.sh
 Daemon agent to pull some content from remote host, and do action after check changes.
-###push\_agent.sh
+### push\_agent.sh
 Daemon agent to push some msg to remote host file.
 
-##sendRawPkt
+## [sendRawPkt](sendRawPkt)
 Example to show how to construct a raw socket packet, with manually setting the mac, ip and udp header.
 
-##service
-Example of a system service script in Debian/Ubuntu system, supporting running complicated command with arbitrary parameters.
 
-##shell
+## shell
 Useful shell scripts.
 
-##system
+## [system](system)
 Some system related tools, such as init a fresh os.
-###getweb.sh
+### getweb.sh
 Simple but powerful script, which can automatically download a website by URL and save it to local directory for offline reference.
-###rm.sh
+### rm.sh
 A much safer rm script, which can automatically backup removed files, in case deleting useful data (beta version).
-###nss
+### nss
 This script will show all ip address inside each network namespace. If give a parameter, then it will show the network namespace containing the key. This is useful for openstack debugging.
+### service
+Example of a system service script in Debian/Ubuntu system, supporting running complicated command with arbitrary parameters.
--- python/gitbook_gen.py
+++ python/gitbook_gen.py
@@ -1,102 +1,134 @@
 # -*- coding: utf-8 -*- 
 """
 This script will generate a gitbook projects based on the given dir.
-The generated dir contains the same structure and have the necessary markdown files.
-Output will can be redirected as the SUMMARY.md.
+The generated dir contains the same structure and the corresponding markdown
+files.
 
-Usage: python gitbook_gen source_code_dir
+Usage: python gitbook_gen [source_dir1] [source_dir2] ...
 
 Author: yeasy@github.com
 """
 
+import __future__
 import os 
 import sys
 
-ROOT_DIR = os.getcwd()
-RESULT_DIR= ROOT_DIR+os.sep+'outline_dir'
+ROOT_PATH = os.getcwd()
+RESULT_PATH = ""
+
 
 def create_file(file_path, content, forced=False):
-    """
-    Create a file at the path, and write into the content.
+    """ Create a file at the path, and write the content.
     If not forced, when file already exists, then do nothing.
+
+    :param file_path: The whole path of the file
+    :param content: Content to write into the file
+    :param forced: Whether to force to overwrite file content
+    :return: None
     """
     if os.path.isfile(file_path) and not forced:
-        print "%s already exists, stop writing content=%s" % (file_path, content)
+        print("Warn: {} already exists, stop writing content={}".format(
+            file_path, content))
         return
     with open(file_path, 'w') as f:
         f.write(content+'\n')
 
-def append_summary(content, debug=False):
+
+def append_summary_file(content, debug=False):
+    """ Append the content into the SUMMARY.md file
+
+    :param content: content to append to SUMMARY file
+    :param debug: Whether to output the result
+    :return: None
     """
-    Write the content into the SUMMARY.md file
-    """
-    summary_path = RESULT_DIR + os.sep + 'SUMMARY.md'
-    if not os.path.isfile(summary_path):
-        create_file(summary_path, content)
+    summary_file = RESULT_PATH + os.sep + 'SUMMARY.md'
+    if not os.path.isfile(summary_file):
+        create_file(summary_file, content)
     else:    
-        with open(summary_path, 'a') as f:
+        with open(summary_file, 'a') as f:
             f.write(content+'\n')
     if debug:
-        print content
+        print(content)
 
-def init_gb_dir(dirname, title):
-    """
-    Initialized a gitbook dir by make sure its existence and init a README.md
-    """
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-    create_file(dirname+os.sep+'README.md', '', forced=True)
 
-def refine_dirname(dirname):
-    """
-    Refine a dirname string to make sure it's validate for processing.
-    """
-    dirname = dirname.replace('.'+os.sep,'') # remove ./
-    if dirname.endswith('/') or dirname.endswith('\\'):
-        dirname = dirname[:-1]
-    return dirname
+def init_gitbook_dir(dir_path, title):
+    """ Initialized a gitbook dir by make sure its existence and init a README.md
 
-def process_dir(rootDir, level=1): 
+    :param dir_path: whole dir path
+    :param title: project title
+    :return:
     """
-    Process the dir, checking sub dirs and files.
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    create_file(dir_path + os.sep + 'README.md',
+                ("{}\n"+"="*len(title)).format(title), forced=True)
+
+
+def refine_dirname(name):
+    """ Refine a dir name to make sure it's validate for processing.
+
+    :param name: directory name to refine
+    :return: refined result
     """
-    if level>4: # do not process very deep dir
+    name = name.replace('.' + os.sep, '')  # remove ./
+    if name.endswith('/') or name.endswith('\\'):
+        name = name[:-1]
+    return name
+
+
+def process_dir(root_dir, level=1):
+    """ Process the dir, checking sub dirs and files.
+
+    :param root_dir:
+    :param level:
+    :return:
+    """
+    if level > 4:  # do not process very deep dir
         return
-    valid_dirs = filter(lambda x: not x.startswith('.'), os.listdir(rootDir))
-    list_dir = filter(lambda x: os.path.isdir(os.path.join(rootDir, x)), valid_dirs)
-    list_file = filter(lambda x: os.path.isfile(os.path.join(rootDir, x)) and not x.startswith('_'), valid_dirs)
+    valid_dirs = filter(lambda x: not x.startswith('.'), os.listdir(root_dir))
+    list_dir = filter(lambda x: os.path.isdir(os.path.join(root_dir, x)),
+                      valid_dirs)
+    list_file = filter(lambda x: os.path.isfile(os.path.join(root_dir, x)) and
+                                 not x.startswith('_'), valid_dirs)
     
-    for e in list_dir: # dirs
-        path = os.path.join(rootDir, e).replace('.'+os.sep,'')
+    for e in list_dir:  # dirs
+        path = os.path.join(root_dir, e).replace('.' + os.sep, '')
         if level == 4:
-            create_file(RESULT_DIR+os.sep+path+'.md', '#'*level+' '+e)
+            create_file(RESULT_PATH + os.sep + path + '.md', '#' * level + ' ' + e)
             line = '* [%s](%s.md)' % (e,path.replace('\\','/'))
         else:
-            if not os.path.exists(RESULT_DIR+os.sep+path):
-                os.makedirs(RESULT_DIR+os.sep+path)
-            create_file(RESULT_DIR+os.sep+path+os.sep+'README.md', '#'*level+' '+e, forced=True)
+            if not os.path.exists(RESULT_PATH+os.sep+path):
+                os.makedirs(RESULT_PATH + os.sep + path)
+            create_file(RESULT_PATH + os.sep + path + os.sep + 'README.md',
+                        '#' * level + ' ' + e, forced=True)
             line = '* [%s](%s/README.md)' % (e, path.replace('\\','/'))
-        append_summary(' '*3*(level-1)+line)
+        append_summary_file(' ' * 3 * (level - 1) + line)
         process_dir(path, level+1)
-    for e in list_file: # files
-        name, suffix = os.path.splitext(e) #test .py
-        path = os.path.join(rootDir, name).replace('.'+os.sep,'') + suffix.replace('.','_') # test\test_py
-        create_file(RESULT_DIR+os.sep+path+'.md', '#'*level+' '+e)
-        line = '* [%s](%s.md)' % (e, path.replace('\\','/'))
-        append_summary(' '*3*(level-1)+line)
+    for e in list_file:  # files
+        name, suffix = os.path.splitext(e)  # test .py
+        path = os.path.join(root_dir, name).replace('.' + os.sep, '') \
+               + suffix.replace('.', '_')  # test\test_py
+        create_file(RESULT_PATH + os.sep + path + '.md', '#' * level + ' ' + e)
+        line = '* [%s](%s.md)' % (e, path.replace('\\', '/'))
+        append_summary_file(' ' * 3 * (level - 1) + line)
+
 
 if __name__ == '__main__':
-    if len(sys.argv)>1:
+    if len(sys.argv) > 1:
         for d in sys.argv[1:]:
             if not os.path.exists(d):
-                print "%s does not exist" %d
+                print("WARN: dir name {} does not exist".format(d))
                 continue
             d = refine_dirname(d)
-            RESULT_DIR = ROOT_DIR+os.sep+d.replace('/','_').replace('\\','_')+'_gitbook'
-            init_gb_dir(RESULT_DIR, d)
-            append_summary("#Summary\n")
+            RESULT_PATH = ROOT_PATH + os.sep \
+                          + d.replace('/', '_').replace('\\', '_') \
+                          + '_gitbook'
+            print("Will init the output dir={}".format(RESULT_PATH))
+            init_gitbook_dir(RESULT_PATH, d)
+            append_summary_file("#Summary\n")
             os.chdir(d)
             process_dir('.')
     else:
-        print "Put the input path(s) as parameters"
+        print("Put the input dir name(s) as parameters")
+        print("Usage: python gitbook_gen [source_dir1] [source_dir2] ... ")
 
